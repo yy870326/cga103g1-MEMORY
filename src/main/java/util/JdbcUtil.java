974e31e1f956620e65bdfae85377b0a15f26c5ea
TLexYuW <Lexcorpax@gmail.com>
2022-08-16 21:26:12 +0800

    First Commit

---
+++ src/main/java/util/JdbcUtil.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * 專案上線使用 JNDI
+ */
+public class JdbcUtil {
+	public static final String URL 
+	= "jdbc:mysql://localhost:3306/cga103g1?useSSL=false&serverTimezone=UTC";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "密碼";
+
+}
+
+	
