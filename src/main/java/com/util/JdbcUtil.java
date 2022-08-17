4493ec341ed7f222f15cd67dcacb80ee45ae06ac
TLexYuW <Lexcorpax@gmail.com>
2022-08-17 19:11:38 +0800

    JdbcUtil 更正

--- src/main/java/com/util/JdbcUtil.java
+++ src/main/java/com/util/JdbcUtil.java
@@ -4,7 +4,7 @@ package com.util;
  * 專案上線使用 JNDI
  */
 public class JdbcUtil {
-	public static final String DRIVER="";
+	public static final String DRIVER="com.mysql.cj.jdbc.Driver";
 	public static final String URL 
 	= "jdbc:mysql://localhost:3306/cga103g1?useSSL=false&serverTimezone=UTC";
 	public static final String USERNAME = "root";
