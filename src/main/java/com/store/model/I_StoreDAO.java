d797950b14800ee48883bf8fd1040ad55fc737f0
Oliver Chen <oliver20220810@gmail.com>
2022-08-18 09:04:37 +0800

    new update store

---
+++ src/main/java/com/store/model/I_StoreDAO.java
@@ -0,0 +1,12 @@
+package com.store.model;
+
+import java.util.List;
+
+public  interface  I_StoreDAO {
+	public void insert(StoreVO storeVO);
+	public void update(StoreVO storeVO);
+	public void delete(Integer store_no);
+	public StoreVO queryStore(Integer store_no);
+	public List<StoreVO> getAll();
+	
+}
