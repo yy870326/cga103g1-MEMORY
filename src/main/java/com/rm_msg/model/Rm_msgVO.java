c23f31c5fa0c83ae4a00a48fae231d1770063895
Oliver Chen <oliver20220810@gmail.com>
2022-08-18 16:55:50 +0800

    上傳 msgDAO store rm_order_list

---
+++ src/main/java/com/rm_msg/model/Rm_msgVO.java
@@ -0,0 +1,64 @@
+package com.rm_msg.model;
+
+import java.sql.Date;
+
+public class Rm_msgVO {
+	private Integer rm_msg_no;
+	private Integer emp_no;
+	private Integer mem_no;
+	private Integer store_no;
+	private String rm_msg_reason;
+	private Date rm_msg_date;
+	private Integer rm_msg_status;
+	private Date rm_msg_donetime;
+	public Integer getRm_msg_no() {
+		return rm_msg_no;
+	}
+	public void setRm_msg_no(Integer rm_msg_no) {
+		this.rm_msg_no = rm_msg_no;
+	}
+	public Integer getEmp_no() {
+		return emp_no;
+	}
+	public void setEmp_no(Integer emp_no) {
+		this.emp_no = emp_no;
+	}
+	public Integer getMem_no() {
+		return mem_no;
+	}
+	public void setMem_no(Integer mem_no) {
+		this.mem_no = mem_no;
+	}
+	public Integer getStore_no() {
+		return store_no;
+	}
+	public void setStore_no(Integer store_no) {
+		this.store_no = store_no;
+	}
+	public String getRm_msg_reason() {
+		return rm_msg_reason;
+	}
+	public void setRm_msg_reason(String rm_msg_reason) {
+		this.rm_msg_reason = rm_msg_reason;
+	}
+	public Date getRm_msg_date() {
+		return rm_msg_date;
+	}
+	public void setRm_msg_date(Date rm_msg_date) {
+		this.rm_msg_date = rm_msg_date;
+	}
+	public Integer getRm_msg_status() {
+		return rm_msg_status;
+	}
+	public void setRm_msg_status(Integer rm_msg_status) {
+		this.rm_msg_status = rm_msg_status;
+	}
+	public Date getRm_msg_donetime() {
+		return rm_msg_donetime;
+	}
+	public void setRm_msg_donetime(Date rm_msg_donetime) {
+		this.rm_msg_donetime = rm_msg_donetime;
+	}
+	
+	
+}
--- src/main/java/com/store/model/StoreService.java
+++ src/main/java/com/store/model/StoreService.java
@@ -1,9 +1,141 @@
 package com.store.model;
 
+import java.sql.Date;
+import java.util.List;
+
+import com.rm_order_list.model.Rm_order_listVO;
+
 public class StoreService {
 	private I_StoreDAO dao;
 	
 	public StoreService() {
 		dao = new StoreDAO();
+	}
+	
+	public StoreVO addStoreService(
+	 String store_acc,
+	 String store_pwd,
+	 Byte acc_status,
+	 String store_name,
+	 String store_gui,
+	 String store_rep,
+	 String store_tel,
+	 String store_fax,
+	 String store_add,
+	 String store_poc,
+	 String store_con_phone,
+	 String store_con_add,
+	 String store_email,
+	 Date store_reg_date,
+	 String bank_account,
+	 Integer store_tkt_rating_scoure,
+	 Integer store_tkt_rating_count,
+	 Integer store_tkt_rating,
+	 Integer store_rm_rating_score,
+	 Integer store_rm_rating_count,
+	 Integer store_act_rating_score,
+	 Integer store_act_rating_count,
+	 Integer store_report_count
+			) {
+		StoreVO storeVO = new StoreVO();
+		
+		storeVO.setStore_acc(store_acc);
+		storeVO.setStore_pwd(store_pwd);
+		storeVO.setAcc_status(acc_status);
+		storeVO.setStore_name(store_name);
+		storeVO.setStore_gui(store_gui);
+		storeVO.setStore_rep(store_rep);
+		storeVO.setStore_tel(store_tel);
+		storeVO.setStore_fax(store_fax);
+		storeVO.setStore_add(store_add);
+		storeVO.setStore_poc(store_poc);
+		storeVO.setStore_con_phone(store_con_phone);
+		storeVO.setStore_con_add(store_con_add);
+		storeVO.setStore_email(store_email);
+		storeVO.setStore_reg_date(store_reg_date);
+		storeVO.setBank_account(bank_account);
+		storeVO.setStore_tkt_rating(store_tkt_rating);
+		storeVO.setStore_tkt_rating_count(store_tkt_rating_count);
+		storeVO.setStore_tkt_rating_scoure(store_tkt_rating_scoure);
+		storeVO.setStore_rm_rating_count(store_rm_rating_count);
+		storeVO.setStore_rm_rating_score(store_rm_rating_score);
+		storeVO.setStore_act_rating_count(store_act_rating_count);
+		storeVO.setStore_act_rating_score(store_act_rating_score);
+		storeVO.setStore_report_count(store_report_count);
+		dao.insert(storeVO);
+		
+		return storeVO;
+	}
+	
+	public StoreVO updateStoreService (
+			
+			 Integer store_no,
+			 String store_acc,
+			 String store_pwd,
+			 Byte acc_status,
+			 String store_name,
+			 String store_gui,
+			 String store_rep,
+			 String store_tel,
+			 String store_fax,
+			 String store_add,
+			 String store_poc,
+			 String store_con_phone,
+			 String store_con_add,
+			 String store_email,
+			 Date store_reg_date,
+			 String bank_account,
+			 Integer store_tkt_rating_scoure,
+			 Integer store_tkt_rating_count,
+			 Integer store_tkt_rating,
+			 Integer store_rm_rating_score,
+			 Integer store_rm_rating_count,
+			 Integer store_act_rating_score,
+			 Integer store_act_rating_count,
+			 Integer store_report_count
+			) {
+		
+		StoreVO storeVO = new StoreVO();
+		
+		storeVO.setStore_no(store_no);
+		storeVO.setStore_acc(store_acc);
+		storeVO.setStore_pwd(store_pwd);
+		storeVO.setAcc_status(acc_status);
+		storeVO.setStore_name(store_name);
+		storeVO.setStore_gui(store_gui);
+		storeVO.setStore_rep(store_rep);
+		storeVO.setStore_tel(store_tel);
+		storeVO.setStore_fax(store_fax);
+		storeVO.setStore_add(store_add);
+		storeVO.setStore_poc(store_poc);
+		storeVO.setStore_con_phone(store_con_phone);
+		storeVO.setStore_con_add(store_con_add);
+		storeVO.setStore_email(store_email);
+		storeVO.setStore_reg_date(store_reg_date);
+		storeVO.setBank_account(bank_account);
+		storeVO.setStore_tkt_rating(store_tkt_rating);
+		storeVO.setStore_tkt_rating_count(store_tkt_rating_count);
+		storeVO.setStore_tkt_rating_scoure(store_tkt_rating_scoure);
+		storeVO.setStore_rm_rating_count(store_rm_rating_count);
+		storeVO.setStore_rm_rating_score(store_rm_rating_score);
+		storeVO.setStore_act_rating_count(store_act_rating_count);
+		storeVO.setStore_act_rating_score(store_act_rating_score);
+		storeVO.setStore_report_count(store_report_count);
+		dao.update(storeVO);
+		
+		return storeVO;
+	} 
+	
+	public void deleteStoreService(Integer store_no) {
+		dao.delete(store_no);
+		
+	}
+	
+	public StoreVO getOneStore(Integer store_no) {
+		return dao.queryStore(store_no);
+	}
+	
+	public List<StoreVO> getAll(){
+		return dao.getAll();
 	}
 }
