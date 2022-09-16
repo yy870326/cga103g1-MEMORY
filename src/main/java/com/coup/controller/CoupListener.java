4340f10094220abd728087c9b7f014bff4d675ce
kaychen <grasekao@gmail.com>
2022-09-17 00:04:59 +0800

    修改會員優惠券排程器

--- src/main/java/com/coup/controller/CoupListener.java
+++ src/main/java/com/coup/controller/CoupListener.java
@@ -1,7 +1,5 @@
 package com.coup.controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -17,23 +15,23 @@ import com.coup.model.CoupService;
 import com.coup.model.CoupVO;
 import com.mem_coup.model.MemCoupService;
 import com.mem_coup.model.MemCoupVO;
-import com.tkt_order2.model.TktOrderMailService;
+
 
 //
 @WebListener
 public class CoupListener implements ServletContextListener {
-	Timer timer = null;
+	Timer timer;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		timer.cancel();
 	}
 
-//	
+	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 
-		Timer timer = new Timer();
+		timer = new Timer();
 		TimerTask task = new TimerTask() {
 
 			@Override
@@ -55,7 +53,6 @@ public class CoupListener implements ServletContextListener {
 					// 如果現在時間超過過期時間就下架
 					if (realEndDay.after(enddate)) {
 						coupSrv.updateCoupStatus(coupVO.getCoup_no());
-//						System.out.println(coupVO.getCoup_no() + "優惠券類型下架"); 
 					}
 				}
 
@@ -72,37 +69,22 @@ public class CoupListener implements ServletContextListener {
 					java.util.Date today = new java.util.Date(); // 現在時間
 					java.util.Date enddate = memCouponVO.getCoupVO().getEnddate(); // 先找優惠券再找
 					java.util.Date realEndDay = new Date(today.getTime() - (1000 * 60 * 60 * 24));
-//					System.out.println(enddate);
 
 					// 如果截止日小於今天代表過期，更改優惠券狀態為 2
 					if (realEndDay.after(enddate)) {
-						memCoupSrv.changeState(memCouponVO.getMem_coup_no(), 2); // 會員優惠券狀態 2 過期
-//						 System.out.println(memCouponVO.getMem_coup_no() + "我的優惠券過期");
+//						memCoupSrv.changeState(memCouponVO.getMem_coup_no(), 2); // 會員優惠券狀態 2 過期
+						memCoupSrv.memCoupOverdueState(memCouponVO.getMem_coup_no());
 					}
-
-					// 要自動抓會員名字 email
-//					String to = tkt_order_mem_email; // 要抓會員 email 或是在購買頁新增欄位傳參數過來
-//					String subject = "票券訂單成功";
-//					String ch_name = tkt_order_mem_name + " 用戶"; // 要抓會員 帳戶名
-//					String messageText = "Hello! " + ch_name + "，您已成功在美陌旅訂購票券，本次的消費金額為 NT$" + ttl_price + " 元，感謝您的購買";
-//
-//					TktOrderMailService tktOrdMailSrv = new TktOrderMailService();
-//					tktOrdMailSrv.sendMail(to, subject, messageText);
 
 				}
 
 			}
 		};
 
-		Calendar cal = new GregorianCalendar(2022, Calendar.SEPTEMBER, 11, 0, 0, 0);// 9/11號 凌晨0點開始
-		timer.scheduleAtFixedRate(task, cal.getTime(), 24 * 60 * 60 * 1000);// 從上面指定時間開始，並在每天執行一次
-//		
-//		Calendar calendar = Calendar.getInstance();
-//		calendar.set(Calendar.HOUR_OF_DAY, 0); // 每天 0 
-//		calendar.set(Calendar.MINUTE, 0);
-//		calendar.set(Calendar.SECOND, 0);
-//		// java.util Date 第一次執行動作的時間是凌晨 0 點
-//		Date date = calendar.getTime();
+//		Calendar cal = new GregorianCalendar(2022, Calendar.SEPTEMBER, 16, 0, 0, 0);// 9/11號 凌晨0點開始
+//		timer.scheduleAtFixedRate(task, cal.getTime(), 24 * 60 * 60 * 1000);// 從上面指定時間開始，並在每天執行一次
+		timer.schedule(task, 1000, 24 * 60 * 60 * 1000);// 從上面指定時間開始，並在每天執行一次
+
 
 	}
 
--- src/main/java/com/mem_coup/model/MemCoupDAO.java
+++ src/main/java/com/mem_coup/model/MemCoupDAO.java
@@ -16,6 +16,8 @@ public class MemCoupDAO implements I_MemCoupDAO {
 
 	private static final String INSERT = "INSERT INTO mem_coup (mem_no, coup_no, coup_state) VALUES (?, ?, 0);";
 	private static final String CHANGE_STATE = "UPDATE mem_coup SET coup_state = ? WHERE mem_coup_no = ?;";
+	private static final String USED_STATE = "UPDATE mem_coup SET coup_state = 1 WHERE mem_coup_no = ?;";
+	private static final String ORVERDUE_STATE = "UPDATE mem_coup SET coup_state = 2 WHERE mem_coup_no = ?;";
 	private static final String GET_ONE = "SELECT mem_coup_no ,mem_no, coup_no, coup_state FROM mem_coup WHERE mem_coup_no = ?;";
 	private static final String GET_ALL = "SELECT mem_coup_no ,mem_no, coup_no, coup_state FROM mem_coup ORDER BY mem_coup_no;";
 	private static final String GET_MEMCOUP_BY_MEMNO = "SELECT mem_coup_no ,mem_no, coup_no, coup_state FROM mem_coup WHERE mem_no =?";
@@ -265,15 +267,15 @@ public class MemCoupDAO implements I_MemCoupDAO {
 	public void changeState(Integer mem_coup_no, Integer coup_state) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		MemCoupVO memCoupVO = null;
+//		MemCoupVO memCoupVO = null;
 		
 		try {
-			memCoupVO = new MemCoupVO();
+//			memCoupVO = new MemCoupVO();
 			con = ds.getConnection();
 			ps = con.prepareStatement(CHANGE_STATE);
 
-			ps.setInt(1, memCoupVO.getCoup_state());
-			ps.setInt(2, memCoupVO.getMem_coup_no());
+			ps.setInt(1, mem_coup_no);
+			ps.setInt(2, coup_state);
 
 			ps.executeUpdate();
 
@@ -299,18 +301,51 @@ public class MemCoupDAO implements I_MemCoupDAO {
 	}
 
 	@Override
-	public void memCoupUsedState(Integer mem_coup_no, Integer coup_state) {
+	public void memCoupUsedState(Integer mem_coup_no) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		MemCoupVO memCoupVO = null;
+//		MemCoupVO memCoupVO = null;
 		
 		try {
-			memCoupVO = new MemCoupVO();
+//			memCoupVO = new MemCoupVO();
 			con = ds.getConnection();
-			ps = con.prepareStatement(CHANGE_STATE);
+			ps = con.prepareStatement(USED_STATE);
 
-			ps.setInt(1, 1);
-			ps.setInt(2, memCoupVO.getMem_coup_no());
+			ps.setInt(1, mem_coup_no);
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void memCoupOverdueState(Integer mem_coup_no) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(ORVERDUE_STATE);
+
+			ps.setInt(1, mem_coup_no);
 
 			ps.executeUpdate();
 
