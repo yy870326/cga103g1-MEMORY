3dd65d7f1ce20fd7111e71519162d7bfd519d061
chgv <chgv0926@gmail.com>
2022-09-22 23:44:06 +0800

    9/22

--- src/main/java/com/mem/model/MemService.java
+++ src/main/java/com/mem/model/MemService.java
@@ -10,9 +10,8 @@ public class MemService {
 		dao = new MemDAO();
 	}
 
-	public MemVO addMem( String mem_acc, String mem_pwd,  String mem_name,
-			String mem_gender, String mem_email, String mem_mobile, String mem_city, String mem_dist, String mem_addr,
-			 byte[] mem_pic,  String mem_card
+	public MemVO addMem(String mem_acc, String mem_pwd, String mem_name, String mem_gender, String mem_email,
+			String mem_mobile, String mem_city, String mem_dist, String mem_addr, byte[] mem_pic, String mem_card
 
 	) {
 		MemVO memVO = new MemVO();
@@ -34,9 +33,10 @@ public class MemService {
 
 		return memVO;
 	}
-	public MemVO updateMem( String mem_acc, String mem_pwd, Integer acc_status, String mem_name,
-			String mem_gender, String mem_email, String mem_mobile, String mem_city, String mem_dist, String mem_addr,
-			Date mem_reg_date, byte[] mem_pic, Integer mem_report_count, String mem_card, Integer mem_no
+
+	public MemVO updateMem(String mem_acc, String mem_pwd, Integer acc_status, String mem_name, String mem_gender,
+			String mem_email, String mem_mobile, String mem_city, String mem_dist, String mem_addr, Date mem_reg_date,
+			byte[] mem_pic, Integer mem_report_count, String mem_card, Integer mem_no
 
 	) {
 		MemVO memVO = new MemVO();
@@ -60,9 +60,9 @@ public class MemService {
 
 		return memVO;
 	}
-	public MemVO update( String mem_pwd, String mem_name,
-			String mem_gender, String mem_email, String mem_mobile, String mem_city, 
-			 Integer mem_no) {
+
+	public MemVO update(String mem_pwd, String mem_name, String mem_gender, String mem_email, String mem_mobile,
+			String mem_city, Integer mem_no) {
 		MemVO memVO = new MemVO();
 
 		memVO.setMem_pwd(mem_pwd);
@@ -76,36 +76,58 @@ public class MemService {
 
 		return memVO;
 	}
+
 	public void deleteMem(Integer memno) {
 		dao.delete(memno);
 	}
+
 	public MemVO getOneMem(Integer memno) {
 		return dao.findByPrimaryKey(memno);
 	}
-	public List<MemVO>getall(){
+
+	public List<MemVO> getall() {
 		return dao.getAll();
 	}
-public MemVO getOneBymail(String mem_email) {
-		
+
+	public MemVO getOneBymail(String mem_email) {
+
 		return dao.getOneBymail(mem_email);
 	}
-public MemVO updatePassword(String mem_password,Integer mem_no) {
-	
-	MemVO memVO = new MemVO();
-	memVO.setMem_pwd(mem_password);
-	memVO.setMem_no(mem_no);
-	
-	dao.upatePassword(memVO);
-	return memVO;
-}
-public MemVO updateStatus(Integer acc_status, Integer mem_no) {
-	MemVO memVO = new MemVO();
-	memVO.setAcc_status(acc_status);
-	memVO.setMem_no(mem_no);
-	dao.updateStatus(memVO);
-	return memVO;
-}
-public MemVO login(String mem_email, String mem_pwd) {
-	return dao.login(mem_email, mem_pwd);
-}
+
+	public MemVO updatePassword(String mem_password, Integer mem_no) {
+
+		MemVO memVO = new MemVO();
+		memVO.setMem_pwd(mem_password);
+		memVO.setMem_no(mem_no);
+
+		dao.upatePassword(memVO);
+		return memVO;
+	}
+
+	public MemVO updateStatus(Integer acc_status, Integer mem_no) {
+		MemVO memVO = new MemVO();
+		memVO.setAcc_status(acc_status);
+		memVO.setMem_no(mem_no);
+		dao.updateStatus(memVO);
+		return memVO;
+	}
+
+	public MemVO login(String mem_email, String mem_pwd) {
+		return dao.login(mem_email, mem_pwd);
+	}
+
+	public MemVO updateMemB(String mem_name, String mem_gender, String mem_email, String mem_mobile, String mem_city,
+			String mem_dist, String mem_addr, Integer mem_no) {
+		MemVO memVO = new MemVO();
+		memVO.setMem_name(mem_name);
+		memVO.setMem_gender(mem_gender);
+		memVO.setMem_email(mem_email);
+		memVO.setMem_mobile(mem_mobile);
+		memVO.setMem_city(mem_city);
+		memVO.setMem_dist(mem_dist);
+		memVO.setMem_addr(mem_addr);
+		memVO.setMem_no(mem_no);
+		dao.update(memVO);
+		return memVO;
+	}
 }
